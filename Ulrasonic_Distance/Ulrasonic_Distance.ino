be4f19241b5b758e1fc3d35c3e2b8c5300d4a6a3
Alec <52588950+Linja82@users.noreply.github.com>
2023-11-29 21:00:34 -0500

    more on ultrasonic

--- Ulrasonic_Distance/Ulrasonic_Distance.ino
+++ Ulrasonic_Distance/Ulrasonic_Distance.ino
@@ -1,7 +1,7 @@
 /* Description:
  * 
- * This code will measure the distance of the object in front of the HC-SR04 ultrasonic distance sensor, then
- * print out the distance in cm in the serial monitor. 
+ *    This code will measure the distance of the object in front of the HC-SR04 ultrasonic distance sensor, then
+ *    print out the distance in cm in the serial monitor. 
  * 
  * Notes: 
  * 
@@ -22,23 +22,35 @@
  *      VCC                 5V
  */ 
 
-#include <NewPing.h>
+////////////////////////////////////////////// Libraries ////////////////////////////////////////////
+#include <NewPing.h>  // Library to use the distance sensor. Documentation: https://bitbucket.org/teckel12/arduino-new-ping/wiki/Home
 
-// Pin definitions
+////////////////////////////////////////// Pin Definitions //////////////////////////////////////////
 #define TRIGGER_PIN 2     // Pin that the trigger pin on the HC-SR04 is connected to
 #define ECHO_PIN 3        // Pin that the echo pin on the HC-SR04 is connected to
 
+/////////////////////////////////// Sonar Configuration Variables ///////////////////////////////////
 #define MAX_DIST  300     // The maximum distance in centimeters that the sensor will try to measure. Anything greater than 300cm will return as 300cm
 #define ITERATIONS 5      // The library will take multiple measurements then return the median value to reduce outliers.
                           // The ITERATION variable sets the number of measurements taken before the median is calculated.
 
-// Data variables
+/////////////////////////////////////////// Data Variables //////////////////////////////////////////
 int distance;             // The variable that stores the measured distance value
 
+///////////////////////////////////////////// Sonar Stuff////////////////////////////////////////////
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DIST);   // Initializes the sensor at the pins specified earlier. Also specifies the maximum distance the sensor will try to measure.
+
+/////////////////////////////////////////// Initialization //////////////////////////////////////////
 void setup() {
   Serial.begin(9600);     // Initializes the serial port at a speed of 9600 baud. This is done so we can receive information from the Arduino.
 }
 
+////////////////////////////////////////////// Main Loop ////////////////////////////////////////////
 void loop() {
+  distance = sonar.ping_median(ITERATIONS);   // Sends out multiple pings (Number of pings equals the ITERATIONS variable), then calculates the median value.
+                                              // This will give the measured time for the ping to return in microseconds. This does not give a reading in a length unit.
 
+  distance = sonar.convert_cm(distance);      // This converts the distance from time to a length unit, more specifically centimeters in this case.
+
+  Serial.println(distance);     // Prints the measured distance to the serial monitor
 }
